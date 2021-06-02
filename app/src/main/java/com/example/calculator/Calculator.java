738438633147925d9a9f1d163578cff962231caf
Dmitry Klochkov <Klochkov.Dm595@yandex.ru>
2021-06-03 02:56:49 +0300

    Init Commit

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,161 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Calculator extends AppCompatActivity {
+    private double firstArg;
+    private double secondArg;
+    private String result;
+    private StringBuilder inputStr = new StringBuilder();
+
+    private State state;
+    private enum State {
+        firstArgInput,
+        operationSelection,
+        secondArgInput,
+        resultShow
+    }
+
+    private Operation operation;
+    private enum Operation {
+        addition,
+        subtraction,
+        division,
+        multiplication,
+        percentage
+    }
+
+    public Calculator() {
+        state = State.firstArgInput;
+    }
+
+    public void onNumPressed(int buttonId) {
+
+        if (inputStr.length() < 10 && (state == State.firstArgInput || state == State.secondArgInput)) {
+
+            if (buttonId == R.id.key_dot) {
+                if (inputStr.length() != 0) {
+                    inputStr.append(".");
+                }
+            }
+            if (buttonId == R.id.key_0) {
+                if (inputStr.toString().equals("0")) {
+                    inputStr.append("0");
+                }
+            }
+            if (buttonId == R.id.key_1) {
+                inputStr.append("1");
+            }
+            if (buttonId == R.id.key_2) {
+                inputStr.append("2");
+            }
+            if (buttonId == R.id.key_3) {
+                inputStr.append("3");
+            }
+            if (buttonId == R.id.key_4) {
+                inputStr.append("4");
+            }
+            if (buttonId == R.id.key_5) {
+                inputStr.append("5");
+            }
+            if (buttonId == R.id.key_6) {
+                inputStr.append("6");
+            }
+            if (buttonId == R.id.key_7) {
+                inputStr.append("7");
+            }
+            if (buttonId == R.id.key_8) {
+                inputStr.append("8");
+            }
+            if (buttonId == R.id.key_9) {
+                inputStr.append("9");
+            }
+        }
+    }
+
+    public void onActionPressed (int buttonId) {
+
+        if (buttonId == R.id.key_ac) {
+            state = State.firstArgInput;
+            inputStr.setLength(0);
+            result = null;
+        }
+
+        if(inputStr.length() != 0) {
+            if (buttonId == R.id.key_addition) {
+                state = State.operationSelection;
+                result = inputStr.toString();
+                firstArg = Double.parseDouble(inputStr.toString());
+                operation = Operation.addition;
+                inputStr.setLength(0);
+                state = State.secondArgInput;
+            }
+            if (buttonId == R.id.key_subtraction) {
+                state = State.operationSelection;
+                result = inputStr.toString();
+                firstArg = Double.parseDouble(inputStr.toString());
+                operation = Operation.subtraction;
+                inputStr.setLength(0);
+                state = State.secondArgInput;
+            }
+            if (buttonId == R.id.key_multiplication) {
+                state = State.operationSelection;
+                result = inputStr.toString();
+                firstArg = Double.parseDouble(inputStr.toString());
+                operation = Operation.multiplication;
+                inputStr.setLength(0);
+                state = State.secondArgInput;
+            }
+            if (buttonId == R.id.key_division) {
+                state = State.operationSelection;
+                result = inputStr.toString();
+                firstArg = Double.parseDouble(inputStr.toString());
+                operation = Operation.division;
+                inputStr.setLength(0);
+                state = State.secondArgInput;
+            }
+            if (buttonId == R.id.key_result) {
+                if (state == State.firstArgInput) {
+                    state = State.resultShow;
+                    firstArg = Double.parseDouble(inputStr.toString());
+                    result = String.format("%.2f", firstArg);
+                    inputStr.setLength(0);
+                    state = State.firstArgInput;
+                } else {
+                    state = State.resultShow;
+                    secondArg = Double.parseDouble(inputStr.toString());
+                    result = String.format("%.2f", calculation(operation, firstArg, secondArg));
+                    inputStr.setLength(0);
+                    state = State.firstArgInput;
+                }
+            }
+            if (buttonId == R.id.key_del) {
+                if (state == State.firstArgInput || state == State.secondArgInput) {
+                    inputStr.setLength(inputStr.length() - 1);
+                    state = State.secondArgInput;
+                }
+            }
+        }
+    }
+
+    public String getInput() {
+       return inputStr.toString();}
+    public String getResult() {
+        return result;}
+
+    public double calculation (Operation o, double firstArg, double secondArg) {
+        double result = 0;
+        if (o == Operation.addition) {
+            result = firstArg + secondArg;
+        }
+        if (o == Operation.subtraction) {
+            result =  firstArg - secondArg;
+        }
+        if (o == Operation.multiplication) {
+            result =  firstArg * secondArg;
+        }
+        if (o == Operation.division) {
+            result =  firstArg / secondArg;
+        } return result;
+    }
+}
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,73 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView view_input;
+    private TextView view_result;
+    private Calculator calculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] numberIds = new int[] {
+                R.id.key_0,
+                R.id.key_1,
+                R.id.key_2,
+                R.id.key_3,
+                R.id.key_4,
+                R.id.key_5,
+                R.id.key_6,
+                R.id.key_7,
+                R.id.key_8,
+                R.id.key_9,
+                R.id.key_dot,
+        };
+
+        int[] actionsIds = new int[] {
+                R.id.key_addition,
+                R.id.key_subtraction,
+                R.id.key_multiplication,
+                R.id.key_division,
+                R.id.key_result,
+                R.id.key_ac,
+                R.id.key_del
+        };
+
+        view_input = findViewById(R.id.view_input);
+        view_result = findViewById(R.id.view_result);
+
+        calculator = new Calculator();
+
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculator.onNumPressed(view.getId());
+                view_input.setText(calculator.getInput());
+            }
+        };
+
+        View.OnClickListener actionButtonOnclickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculator.onActionPressed(view.getId());
+                view_input.setText(calculator.getInput());
+                view_result.setText(calculator.getResult());
+            }
+        };
+
+        for (int numberId : numberIds) {
+            findViewById(numberId).setOnClickListener(numberButtonClickListener);
+        }
+
+        for (int actionsId : actionsIds) {
+            findViewById(actionsId).setOnClickListener(actionButtonOnclickListener);
+        }
+
     }
 }
